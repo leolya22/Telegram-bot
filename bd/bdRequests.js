3cbcf1614875ce587a3e1b6a622d5831d7d646b2
Leonardo Lyapunov <110344004+leolya22@users.noreply.github.com>
2024-04-12 09:18:20 -0300

    cambio de nombre de tabla

--- bd/bdRequests.js
+++ bd/bdRequests.js
@@ -3,35 +3,35 @@ import { sqlRequest } from "./helpers/sqlRequest.js";
 
 export const deleteByChatIdAndEmp = async ( chat_id, empresa ) => {
     await sqlRequest( 
-        `delete from telegramUsuarios where chat_id = '${ chat_id }'
+        `delete from telegram_usuarios where chat_id = '${ chat_id }'
         and EmpId = '${ empresa.EmpId }' and Usuario = '${ empresa.Usuario }'`
     );
 }
 
 export const selectByEmpAndChatId = async ( EmpId, Usuario, chat_id ) => {
     return await sqlRequest( 
-        `select * from telegramUsuarios where EmpId = '${ EmpId }' 
+        `select * from telegram_usuarios where EmpId = '${ EmpId }' 
         and Usuario = '${ Usuario }' and chat_id = '${ chat_id }'`
     );
 }
 
 export const updateJWTandDobleFactor = async ( dobleFactor, EmpId, Usuario ) => {
     await sqlRequest( 
-        `update telegramUsuarios set codigo_doble_factor = '${ dobleFactor }' 
+        `update telegram_usuarios set codigo_doble_factor = '${ dobleFactor }' 
         where EmpId = '${ EmpId }' and Usuario = '${ Usuario }' and chat_id = ''`
     );
 }
 
 export const insertEmp = async ( dobleFactor, EmpId, Usuario) => {
     await sqlRequest( 
-        `insert into telegramUsuarios ( EmpId, Usuario, chat_id, allow_telegram_notif, codigo_doble_factor )
+        `insert into telegram_usuarios ( EmpId, Usuario, chat_id, allow_telegram_notif, codigo_doble_factor )
         Values ( '${ EmpId }', '${ Usuario }', '', 'N', '${ dobleFactor }' )`
     );
 }
 
 export const vincularEmp = async ( chat_id, EmpId, Usuario, text ) => {
     await sqlRequest( 
-        `update telegramUsuarios set chat_id = '${ chat_id }', 
+        `update telegram_usuarios set chat_id = '${ chat_id }', 
         allow_telegram_notif = 'S' where EmpId = '${ EmpId }' 
         and Usuario = '${ Usuario }' and codigo_doble_factor = '${ text }'`
     );
@@ -39,19 +39,19 @@ export const vincularEmp = async ( chat_id, EmpId, Usuario, text ) => {
 
 export const allowNotifications = async ( chat_id ) => {
     await sqlRequest( 
-        `update telegramUsuarios set allow_telegram_notif = 'S' where chat_id = '${ chat_id }'`
+        `update telegram_usuarios set allow_telegram_notif = 'S' where chat_id = '${ chat_id }'`
     );
 }
 
 export const blockNotifications = async ( chat_id ) => {
     await sqlRequest( 
-        `update telegramUsuarios set allow_telegram_notif = 'N' where chat_id = '${ chat_id }'`
+        `update telegram_usuarios set allow_telegram_notif = 'N' where chat_id = '${ chat_id }'`
     );
 }
 
 export const selectAllByChatId = async ( chat_id ) => {
     return await sqlRequest(
-        `select * from telegramUsuarios where chat_id = '${ chat_id }'`
+        `select * from telegram_usuarios where chat_id = '${ chat_id }'`
     );
 }
 
